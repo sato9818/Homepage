7630dc4139fb8aabd86e51be5a7a8cb5d4aa1928
Koki Sato <k.sato@SugawaraLab-MacbookPro-12.local>
2022-03-14 00:08:53 -0700

    add dockerfile

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  // ... rest of the configuration.
+  experimental: {
+    outputStandalone: true,
+  },
+}
